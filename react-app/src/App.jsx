112d63604887428d2f074ec55505f902fcda153a
Muhammad Bilal <mb16837157@gmail.com>
2026-07-26 22:10:11 +0500

    react

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -5,8 +5,8 @@ import React from 'react'
 const App = () => {
   return (
     <>
-    <Navbar/>
     <Card />
+    
     </>
   )
 }
--- react-app/src/components/card.jsx
+++ react-app/src/components/card.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 const card = () => {
   return (
-    <div>
-      <p>Hi, i'm a React card!</p>
-    </div>
+    <>
+      <div className="parent">
+        <div className="card">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtonLFQ9AKmsEhe2MmmGBRTiHK5NzD4aDoTVJRztNnrg&s=10" alt="" />
+            <h1>React JS</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, modi.</p>
+            <button>Buy Now</button>
+        </div>
+      </div>
+    </>
   )
 }
 
