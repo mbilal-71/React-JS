a38d663f6863f81c07c588c3b82e32318a80c87e
Muhammad Bilal <mb16837157@gmail.com>
2026-07-26 19:39:23 +0500

    react

---
+++ react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import Card from "./components/card";
+import Navbar from "./components/navbar";
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+    <Card />
+    </>
+  )
+}
+
+export default App
---
+++ react-app/src/components/card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>
+      <p>Hi, i'm a React card!</p>
+    </div>
+  )
+}
+
+export default card
