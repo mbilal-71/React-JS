3563593a16beb749f6c4993d9ba1245e01e21f8e
Muhammad Bilal <mb16837157@gmail.com>
2026-07-31 18:39:39 +0500

    react

--- props-drilling-02/src/assets/components/card.jsx
+++ props-drilling-02/src/assets/components/card.jsx
@@ -1,34 +1,33 @@
-import React from 'react'
 import { Bookmark } from "lucide-react";
-const card = () => {
-  return (
-   
-   <div className="parent">
-     
-      <div className="card">
-        <div className="top">
-           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ32Ua8H2HcpuzNhQAuWmJ-__qG12Y0wrOiu8inOZJ05Q&s=10" alt="" />
-            <button>Save <Bookmark size={15} color='#3333' /></button>
-        </div>
-        <div className="center">
-            <h3>Amazon <span>5 days ago</span></h3>
-            <h2>Senior UI & UX Designer</h2>
-            <div className='tag'>
-                <h4>Part Time</h4>
-                <h4>Senior Level</h4>
+const card = (props) => {
+    return (
+
+
+
+        <div className="card">
+            <div className="top">
+                <img src={props.brandLogo} alt="" />
+                <button>Save <Bookmark size={15} color='#3333' /></button>
             </div>
-        </div>
-        <div className="bottom">
-            <div>
-                <h3>$120/hr</h3>
-                <p> Lhr</p>
+            <div className="center">
+                <h3>{props.companyName} <span>{props.datePosted}</span></h3>
+                <h2>{props.post}</h2>
+                <div className='tag'>
+                    <h4>{props.tag1}</h4>
+                    <h4>{props.tag2}</h4>
+                </div>
             </div>
+            <div className="bottom">
+                <div>
+                    <h3>{props.pay}</h3>
+                    <p>{props.location}</p>
+                </div>
                 <button>Apply Now</button>
+            </div>
         </div>
-     </div>
-    </div>
-  
-  )
+
+
+    )
 }
 
 export default card
