e5c370bd3f0613cc63c7293765ab7fae18bd2191
Muhammad Bilal <m.bilalferoz1@gmail.com>
2026-08-03 20:19:22 +0500

    hooks

---
+++ hooks/src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+useState
+const App = () => {
+  // const [num, setNum] = useState(0)
+
+
+  // const increaseNum = () => {
+  //   setNum(num + 1)
+
+  // }
+  // const decreaseNum = () => {
+  //   setNum(num - 1)
+
+  // }
+
+
+
+  // return (
+  //   <div>
+  //     <h1>{num}  </h1>
+
+  //     <button onClick={increaseNum}>Increase</button>
+  //     <button onClick={decreaseNum}>Decrease</button>
+  //   </div>
+  // )
+
+
+}
+
+export default App
