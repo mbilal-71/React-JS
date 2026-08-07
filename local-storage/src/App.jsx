111bc497acca907a366b6bb83d53494fe86c04ab
Muhammad Bilal <m.bilalferoz1@gmail.com>
2026-08-07 13:55:42 +0500

    local-storage

--- local-storage/src/App.jsx
+++ local-storage/src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  // localStorage.setItem('user', 'Ali')
+  // const user = localStorage.getItem('user')
+  // const user = localStorage.removeItem('user')
+  // localStorage.clear()
+  // console.log(user)
+
+  // const user = {
+  //   user: "Ahmed",
+  //   age: 19,
+  //   city: 'Lhr'
+  // }
+  // localStorage.setItem('user', JSON.stringify(user))
+  // const user = localStorage.getItem('user')
+  // const user = JSON.parse(localStorage.getItem('user'))
+  // console.log(user)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+
+    </div>
   )
 }
 
