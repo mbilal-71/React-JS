d346c68ff07633942cfa0f766d4c3ce55df124f4
Muhammad Bilal <m.bilalferoz1@gmail.com>
2026-08-07 16:10:00 +0500

    api calling

--- api-calling/src/App.jsx
+++ api-calling/src/App.jsx
@@ -1,35 +1,52 @@
+import axios from 'axios'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  // async function getData() {
+  //   const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+  //   console.log(response)
+  // }
+
+  // const getData = async () => {
+  //   const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+  //   console.log(response)
+  //   console.log(response.data) xx data nhi dy ga await krwana pry ga
+  //   const data = await response.json();
+  //   console.log(data)
+
+  // }
+
+  //npm i axios run in terminal and them import on top
+
+  // const getData = async () => {
+  // const response = await axios.get('https://jsonplaceholder.typicode.com/todos')
+  // const { data } = await axios.get('https://jsonplaceholder.typicode.com/todos')
+  // console.log(response)
+  // console.log(response.data) //direct data dy dy ga
+  // console.log(data)
+
+  const [data, setData] = useState([])
+
+  const getData = async () => {
+
+    const response = await axios.get('https://picsum.photos/v2/list')
+    // console.log(response.data)
+    setData(response.data)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <button onClick={getData}>Get Data</button>
+      {/* <div>
+        {data.map(function (elem, idx) {
+
+          return <h3>Hello, {elem.url} {idx}</h3>
+        })}
+      </div> */}
+    </div>
   )
 }
+
 
 export default App
