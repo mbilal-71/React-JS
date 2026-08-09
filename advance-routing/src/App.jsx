0c23ed8d5aa4429c167b6a03527c166f4c39eec5
Muhammad Bilal <m.bilalferoz1@gmail.com>
2026-08-09 23:50:15 +0500

    routing

--- advance-routing/src/App.jsx
+++ advance-routing/src/App.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import { Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import NotFound from './pages/NotFound'
+import Products from './pages/Products'
+import Men from './pages/Men'
+
 
 const App = () => {
   return (
     <div className='h-screen bg-black text-white' >
       <Navbar />
-      <Footer />
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/products/mens' element={<Men />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </div>
   )
 }
--- advance-routing/src/components/Navbar.jsx
+++ advance-routing/src/components/Navbar.jsx
@@ -5,9 +5,10 @@ const Navbar = () => {
         <div className='flex justify-between bg-cyan-700 h-15 px-5 py-4 font-bold text-lg'>
             <h2 className='text-xl'>Devsinc</h2>
             <div className='flex gap-8'>
-                <Link>Home</Link>
-                <Link>About</Link>
-                <Link>Contact</Link>
+                <Link to='/'>Home</Link>
+                <Link to='/about'>About</Link>
+                <Link to='/contact'>Contact</Link>
+                <Link to='/products'>Products</Link>
             </div>
         </div>
     )
