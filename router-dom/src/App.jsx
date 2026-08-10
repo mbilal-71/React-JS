7cd7c955c2a5cff1942e1512e1867ed59a5c5ea7
Muhammad Bilal <m.bilalferoz1@gmail.com>
2026-08-11 00:39:25 +0500

    routing

--- router-dom/src/App.jsx
+++ router-dom/src/App.jsx
@@ -1,13 +1,17 @@
 import { Route, Routes } from 'react-router-dom'
+import React, { useState } from 'react'
+
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import Navbar from './components/Navbar'
 
 const App = () => {
+  const [theme, setTheme] = useState('Dark')
   return (
     <div>
-      <Navbar />
+      <h2>Theme is {theme}</h2>
+      <Navbar theme={theme} setTheme={setTheme} />
       {/* <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
--- router-dom/src/components/Navbar.jsx
+++ router-dom/src/components/Navbar.jsx
@@ -1,14 +1,15 @@
-import React, { useState } from 'react'
 
 import { Link } from 'react-router-dom'
-const Navbar = () => {
-    const [theme, setTheme] = useState('Dark')
+
+const Navbar = (props) => {
+
 
     return (
         <div className='nav'>
-            <h2>Theme is {theme}</h2>
             <div className='tags'>
-                <button>
+                <button onClick={() => {
+                    props.setTheme('Light')
+                }}>
                     Change Theme
                 </button>
             </div>
